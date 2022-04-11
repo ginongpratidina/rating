7ac498c5a3f9104670427b62692d244af3e44cb5
Dey Raka <dey.raka53@gmail.com>
2022-04-11 15:03:45 +0700

    CRUD API done

--- app/Models/Rating.php
+++ app/Models/Rating.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Model;
 class Rating extends Model
 {
     use HasFactory;
+
+    protected $guarded = [];
+
+    // protected $fillable = ['user_id', 'email_cust'];
 }
